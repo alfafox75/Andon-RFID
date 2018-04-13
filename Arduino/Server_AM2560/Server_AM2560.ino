f4214965cde53bdcbe36e7a51cdb8517167cfb28
alfafox75 <37150810+alfafox75@users.noreply.github.com>
2018-04-13 12:23:45 +0200

    aggiunto timer watchdog attesa richiesta client

--- Arduino/Server_AM2560/Server_AM2560.ino
+++ Arduino/Server_AM2560/Server_AM2560.ino
@@ -11,6 +11,10 @@
 #include "utility/socket.h"
 #include <avr/io.h>
 #include <avr/wdt.h>
+#include <SimpleTimer.h>
+
+// Timer
+  SimpleTimer timer1; // timer richiesta stato
 
 #define Reset_AVR() wdt_enable(WDTO_30MS); while(1) {}
 
@@ -24,6 +28,7 @@ String dataString = "";
 String readString, readString1;
 int x=0;
 char lf=10;
+int T=0;
 
  
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
@@ -35,10 +40,10 @@ IPAddress ip(192, 168, 2, 67);
 IPAddress gateway(192, 168, 2, 1);
 IPAddress subnet(255, 255, 255, 0);
 
-//IPAddress server(10, 162, 128, 65);
-//int port(8088);
-IPAddress server(192, 168, 2, 97);
-int port(80);
+IPAddress server(10, 162, 128, 65);
+int port(8088);
+//IPAddress server(192, 168, 2, 97);
+//int port(80);
  
 EthernetClient client;
 
@@ -58,6 +63,8 @@ void setup()
   digitalWrite(4,HIGH);
   digitalWrite(5,HIGH);
   digitalWrite(10,HIGH);
+
+  timer1.setInterval(60000, WatchDog);
   
   Serial.begin(9600);
   bluetooth.begin(9600);
@@ -71,15 +78,15 @@ void setup()
   
   Serial.println("LoRa Gateway 2 Web Service  --");
 
-//  if (Ethernet.begin(mac) == 0)
-//  {
-//    Serial.println("Configurazione DHCP fallita!");
+  if (Ethernet.begin(mac) == 0)
+  {
+    Serial.println("Configurazione DHCP fallita!");
    Ethernet.begin(mac, ip, gateway, gateway, subnet); 
-//  }
-//  else
-//  {
-//    Serial.println("Configurazione DHCP OK!");
-//  }
+  }
+  else
+  {
+    Serial.println("Configurazione DHCP OK!");
+  }
   Serial.println(Ethernet.localIP());
   Serial.println(server);
   Serial.println(port);
@@ -126,98 +133,100 @@ uint16_t recdata( unsigned char* recbuf, int Length)
 
 void(* Riavvia)(void) = 0;
 
-void loop()
-{
-    if (rf95.waitAvailableTimeout(2000))// Listen Data from LoRa Node
+void loop() {
+  timer1.run();
+  if (rf95.waitAvailableTimeout(2000))// Listen Data from LoRa Node
     {
-        uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];//receive data buffer
-        uint8_t len = sizeof(buf);//data buffer length
-        if (rf95.recv(buf, &len))//Check if there is incoming data
+    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];//receive data buffer
+    uint8_t len = sizeof(buf);//data buffer length
+    if (rf95.recv(buf, &len))//Check if there is incoming data
+      {
+        T=0;
+        recdata( buf, len);
+        Serial.print("Data length: ");
+        Serial.println(len);
+        Serial.print("Get LoRa Packet: ");
+        for (int i = 0; i < len; i++)
         {
-            recdata( buf, len);
-            Serial.print("Data length: ");
-            Serial.println(len);
-            Serial.print("Get LoRa Packet: ");
-            for (int i = 0; i < len; i++)
+            Serial.print(buf[i],HEX);
+            Serial.print(" ");
+        }
+        Serial.println();
+        if(crcdata == recCRCData) //Check if CRC is correct
+        { 
+            if(buf[0] == 1||buf[1] == 0||buf[2] ==0) //Check if the ID match the LoRa Node ID 100
             {
-                Serial.print(buf[i],HEX);
-                Serial.print(" ");
-            }
-            Serial.println();
-            if(crcdata == recCRCData) //Check if CRC is correct
-            { 
-                if(buf[0] == 1||buf[1] == 0||buf[2] ==0) //Check if the ID match the LoRa Node ID 100
-                {
-                    char data[] = "";//Reply 
-                    
-                    String client_id = "";
-                    String user_id = "";
-                    String state_id = "";
-                    
-                    for (int i = 3; i < 17; i++) {
-                      client_id += char(buf[i]);
-                    }
-                    Serial.print("Client ID: ");
-                    Serial.println(client_id);
-
-                    if (len >=20) {
-                      for (int i = 17; i < 31; i++) {
-                        user_id += char(buf[i]);
-                      }
-                      Serial.print("User   ID: ");
-                      Serial.println(user_id);
-  
-                      for (int i = 31; i < 32; i++) {
-                        state_id += char(buf[i]);
-                      }
-                      Serial.print("State  ID: ");
-                      Serial.println(state_id);
-  
-                      data[3] = uploadData(client_id, user_id, state_id);
-                      Serial.print("Response: ");
-                      for (int i = 0; i < 3; i++) {
-                        data[i] = (buf[i]);
-                        Serial.print(data[i], HEX);
-                      }
-                      Serial.print(data[3]);
-                      Serial.println();
-                      
-                      rf95.send(data, 6);// Send Reply to LoRa Node
-                      rf95.waitPacketSent();
-                    }
-                    else {
-                      data[3] = requestState(client_id);
-                      Serial.print("Response: ");
-                      for (int i = 0; i < 3; i++) {
-                        data[i] = (buf[i]);
-                        Serial.print(data[i], HEX);
-                      }
-                      Serial.print(data[3]);
-                      Serial.println();
-                      
-                      rf95.send(data, 6);// Send Reply to LoRa Node
-                      rf95.waitPacketSent();
-                    }
+                char data[] = "";//Reply 
+                
+                String client_id = "";
+                String user_id = "";
+                String state_id = "";
+                
+                for (int i = 3; i < 17; i++) {
+                  client_id += char(buf[i]);
                 }
-            } 
-            else 
-              Serial.println(" CRC Fail");     
-         }
-         else
-         {
-              Serial.println("recv failed");
-              ;
-          }
-      
+                Serial.print("Client ID: ");
+                Serial.println(client_id);
+
+                if (len >=20) {
+                  for (int i = 17; i < 31; i++) {
+                    user_id += char(buf[i]);
+                  }
+                  Serial.print("User   ID: ");
+                  Serial.println(user_id);
+
+                  for (int i = 31; i < 32; i++) {
+                    state_id += char(buf[i]);
+                  }
+                  Serial.print("State  ID: ");
+                  Serial.println(state_id);
+
+                  data[3] = uploadData(client_id, user_id, state_id);
+                  Serial.print("Response: ");
+                  for (int i = 0; i < 3; i++) {
+                    data[i] = (buf[i]);
+                    Serial.print(data[i], HEX);
+                  }
+                  Serial.print(data[3]);
+                  Serial.println();
+                  
+                  rf95.send(data, 6);// Send Reply to LoRa Node
+                  rf95.waitPacketSent();
+                }
+                else {
+                  data[3] = requestState(client_id);
+                  Serial.print("Response: ");
+                  for (int i = 0; i < 3; i++) {
+                    data[i] = (buf[i]);
+                    Serial.print(data[i], HEX);
+                  }
+                  Serial.print(data[3]);
+                  Serial.println();
+                  
+                  rf95.send(data, 6);// Send Reply to LoRa Node
+                  rf95.waitPacketSent();
+                }
+                Serial.println("==================");
+                Serial.println();
+            }
+        } 
+        else 
+          Serial.println(" CRC Fail");     
+     }
+     else
+     {
+          Serial.println("recv failed");
+          ;
       }
+  
+  }
 }
 
-char requestState(String client_id) 
-{
+char requestState(String client_id) {
   char c;
   char cr;
-//  String upload_url = "GET /handleRequestBox.php?box=";
-  String upload_url = "GET /TagManager/handleRequestBox.php?box=";
+  String upload_url = "GET /handleRequestBox.php?box=";
+//  String upload_url = "GET /TagManager/handleRequestBox.php?box=";
   upload_url += client_id;
   upload_url += "&color=1 HTTP/1.1";
   Serial.print(server);
@@ -246,7 +255,7 @@ char requestState(String client_id)
     while(client.connected() && !client.available()) delay(1); //waits for data
     while (client.connected() || client.available()) { //connected or data available
       c = client.read(); //gets byte from ethernet buffer
-      Serial.print(c);
+//      Serial.print(c);
       if (stwr == true) {
         if (c=='@') {
           break; 
@@ -260,8 +269,6 @@ char requestState(String client_id)
     }
     Serial.println();
     Serial.println("disconnecting.");
-    Serial.println("==================");
-    Serial.println();
     client.stop(); //stop client
     
     return cr;
@@ -270,8 +277,8 @@ char requestState(String client_id)
 char uploadData(String client_id, String user_id, String state_id) {//Upload Data to ThingSpeak
   char c;
   char cr;
-//  String upload_url = "GET /handleRequestBox.php?box=";
-  String upload_url = "GET /TagManager/handleRequestBox.php?box=";
+  String upload_url = "GET /handleRequestBox.php?box=";
+//  String upload_url = "GET /TagManager/handleRequestBox.php?box=";
   upload_url += client_id;
   upload_url += "&user=";
   upload_url += user_id;
@@ -300,7 +307,7 @@ char uploadData(String client_id, String user_id, String state_id) {//Upload Dat
     while(client.connected() && !client.available()) delay(1); //waits for data
     while (client.connected() || client.available()) { //connected or data available
       c = client.read(); //gets byte from ethernet buffer
-      Serial.print(c);
+//      Serial.print(c);
       if (stwr == true) {
         if (c=='@') {
           break; 
@@ -314,9 +321,17 @@ char uploadData(String client_id, String user_id, String state_id) {//Upload Dat
     }
     Serial.println();
     Serial.println("disconnecting.");
-    Serial.println("==================");
-    Serial.println();
+    
     client.stop(); //stop client
     
     return cr;
 }
+
+void WatchDog() {
+  T++;
+  Serial.println(T);
+  if (T==3) {
+    Reset_AVR();
+  }
+}
+
